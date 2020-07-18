57209764e92cbabe6e03b2ad002e9a94c97c2e38
kreuzerk <kevin.kreuzer90@icloud.com>
2020-07-18 13:46:38 +0200

    feate(validator): add blue validator

---
+++ src/app/validators/blue.directive.ts
@@ -0,0 +1,6 @@
+import {AbstractControl, ValidatorFn} from '@angular/forms';
+
+export function blue(): ValidatorFn {
+  return (control: AbstractControl): { [key: string]: any } | null =>
+    control.value.toLowerCase() === 'blue' ? null : {wrongColor: {value: control.value}};
+}
